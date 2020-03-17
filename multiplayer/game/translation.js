e4d8f30f39cb6716640acaf20be35e3f78f57c52
WolfgangVonL <WolfgangVonL@users.noreply.github.com>
2020-03-17 16:35:37 +0100

    Update translation.js

--- multiplayer/game/translation.js
+++ multiplayer/game/translation.js
@@ -1,108 +1,142 @@
 var TRANSLATION_MAP = {
   "alles zurück auf die Hand":{
-    "english":"Unset selected letters"
+    "english":"Unset selected letters",
+    "french":"Retirer les lettres sélectionnées"
   },
   "Buchstaben tauschen (passen)":{
     "german":"passen",
-    "english":"skip turn"
+    "english":"skip turn",
+    "french":"Passer son tour"
   },
   "Übrige Buchstaben":{
-    "english":"Letters left in stash"
+    "english":"Letters left in stash",
+    "french":"Lettres restantes"
   },
   "Deine Buchstaben":{
-    "english":"Your letters"
+    "english":"Your letters",
+    "french":"Vos lettres"
   },
   "spielen":{
-    "english":"set letters"
+    "english":"set letters",
+    "french":"placer les lettres"
   },
   "Dreifacher Buchstabenwert":{
-    "english":"triple letter value"
+    "english":"triple letter value",
+    "french":"lettre compte triple"
   },
   "Doppelter Buchtstabenwert":{
-    "english":"double letter value"
+    "english":"double letter value",
+    "french":"lettre compte double"
   },
   "Dreifacher Wortwert":{
-    "english":"triple word value"
+    "english":"triple word value",
+    "french":"mot compte triple"
   },
   "Doppelter Wortwert":{
-    "english":"double word value"
+    "english":"double word value",
+    "french":"mot compte double"
   },
   "Welchen Buchstaben möchtest du hier setzen?":{
-    "english":"Which letter do you want to set?"
+    "english":"Which letter do you want to set?",
+    "french":"Quelle lettre souhaitez-vous placer ?"
   },
   "DU":{
-    "english":"YOU"
+    "english":"YOU",
+    "french":"Vous"
   },
   "KI":{
-    "english":"AI"
+    "english":"AI",
+    "french":"Ordinateur"
   },
   "Wähle die Buchstaben aus, welche Du tauschen möchtest, dann klicke hier":{
-    "english":"Choose the letters you want to swap, then click here"
+    "english":"Choose the letters you want to swap, then click here",
+    "french":"Sélectionnez les lettres que vous souhaitez échanger, puis cliquez ici"
   },
   "Das Spiel ist aus.":{
-    "english":"Game is over."
+    "english":"Game is over.",
+    "french":"Le jeu est fini."
   },
   "Wähle die Stärke des Computers":{
-    "english":"Choose the strength of the ai"
+    "english":"Choose the strength of the ai",
+    "french":"Choisir la difficulté de l'ordinateur"
   },
   "schwach":{
-    "english":"weak"
+    "english":"weak",
+    "french":"facile"
   },
   "normal":{
-    "english":"normal"
+    "english":"normal",
+    "french":"normal"
   },
   "stark":{
-    "english":"strong"
+    "english":"strong",
+    "french":"difficile"
   },
   "sehr stark":{
-    "english":"very strong"
+    "english":"very strong",
+    "french":"très difficile"
   },
   "sehr sehr stark":{
-    "english":"very very strong"
+    "english":"very very strong",
+    "french":"très très difficile"
   },
   "unbesiegbar":{
-    "english":"unbeatable"
+    "english":"unbeatable",
+    "french":"imbattable"
   },
   "für":{
-    "english":"for"
+    "english":"for",
+    "french":"pour"
   },
   "punkte":{
-    "english":"points"
+    "english":"points",
+    "french":"points"
   },
   "Legen des ersten Wortes":{
-    "english":"Inserting the first word"
+    "english":"Inserting the first word",
+    "french":"Placer le premier mot"
   },
   "Der erste Spieler bildet ein Wort aus mindestens zwei Buchstaben. Er platziert es so auf den Spielplan, dass ein Spielstein das Feld in der Mitte des Spielplans bedeckt. Wörter können waagerecht oder senkrecht gelegt werden. Wörter dürfen nicht diagonal gelegt werden. Alle nachfolgenden Buchstaben müssen in einer Linie durchgehend entweder waagerecht oder senkrecht gelegt werden.":{
     "german":"Bilde ein Wort aus mindestens zwei Buchstaben und platziere es so auf den Spielplan, dass ein Spielstein das Feld in der Mitte des Spielplans bedeckt. Wörter können waagerecht oder senkrecht gelegt werden. Wörter dürfen nicht diagonal gelegt werden. Alle nachfolgenden Buchstaben müssen in einer Linie durchgehend entweder waagerecht oder senkrecht gelegt werden.",
-    "english":"Form a word of at least two letters and place it on the board in such a way that a tile covers the field in the middle of the board. Words can be placed horizontally or vertically. Words must not be placed diagonally. All subsequent letters must be placed in a continuous line, either horizontally or vertically."
+    "english":"Form a word of at least two letters and place it on the board in such a way that a tile covers the field in the middle of the board. Words can be placed horizontally or vertically. Words must not be placed diagonally. All subsequent letters must be placed in a continuous line, either horizontally or vertically.",
+    "french":"Formez un mot d'au moins deux lettres et placez-le sur le plateau de manière à ce qu'une tuile couvre le champ au milieu du plateau. Les mots peuvent être placés horizontalement ou verticalement. Les mots ne doivent pas être placés en diagonale. Toutes les lettres suivantes doivent être placées sur une ligne continue, horizontalement ou verticalement."
   },
   "Aufgabe der Spieler ist es, aus Buchstabensteinen mit unterschiedlichem Wert Wörter zusammenzusetzen und so auszulegen, dass sie nach Art eines Kreuzworträtsels miteinander in Verbindung stehen. Jeder Spieler muss darum bemüht sein, eine möglichst hohe Punktzahl zu erzielen, indem er aus den zur Verfügung stehenden Buchstaben Wörter bildet, deren Buchstabenwert in Kombination mit den Prämienfeldern des Spielplans eine möglichst hohe Punktzahl ergeben. Die in einer Partie erreichbare Gesamtpunktzahl liegt bei etwa 400 bis 800 und mehr, je nach dem spielerischen Können der Beteiligten.":{
     "german":"Aufgabe der Spieler ist es, aus Buchstabensteinen mit unterschiedlichem Wert Wörter zusammenzusetzen und so auszulegen, dass sie nach Art eines Kreuzworträtsels miteinander in Verbindung stehen. Jeder Spieler muss darum bemüht sein, eine möglichst hohe Punktzahl zu erzielen, indem er aus den zur Verfügung stehenden Buchstaben Wörter bildet, deren Buchstabenwert in Kombination mit den Prämienfeldern des Spielplans eine möglichst hohe Punktzahl ergeben. Dabei gibt es 50 extra Punkte, wenn alle 7 Buchstabensteine von der Hand auf einmal benutzt werden. Die in einer Partie erreichbare Gesamtpunktzahl liegt bei etwa 400 bis 800 und mehr, je nach dem spielerischen Können der Beteiligten.",
-    "english":"The task of the players is to construct words from letter stones with different values and to interpret them in such a way that they are connected with each other according to the type of a crossword puzzle. Each player must strive to achieve the highest possible score by making words out of the available letters, the letter value of which, in combination with the bonus fields of the game plan, results in the highest possible score. There are 50 extra points if all 7 letters from your hand are used at once. The total score that can be achieved in a game is about 400 to 800 and more, depending on the players' ability to play."
+    "english":"The task of the players is to construct words from letter stones with different values and to interpret them in such a way that they are connected with each other according to the type of a crossword puzzle. Each player must strive to achieve the highest possible score by making words out of the available letters, the letter value of which, in combination with the bonus fields of the game plan, results in the highest possible score. There are 50 extra points if all 7 letters from your hand are used at once. The total score that can be achieved in a game is about 400 to 800 and more, depending on the players' ability to play.",
+    "french":"La tâche des joueurs est de construire des mots à partir de pierres à lettres de valeurs différentes et de les interpréter de manière à ce qu'ils soient connectés les uns aux autres selon le type de mots croisés. Chaque joueur doit s'efforcer d'obtenir le score le plus élevé possible en créant des mots à partir des lettres disponibles, dont la valeur des lettres, en combinaison avec les champs bonus du plan de match, donne le score le plus élevé possible. Il y a 50 points supplémentaires si les 7 lettres de votre main sont utilisées en même temps. Le score total qui peut être atteint dans un jeu est d'environ 400 à 800 et plus, selon la capacité des joueurs à jouer."
   },
   "Felder für Buchstabenprämien":{
-    "english":"Letter Award Fields"
+    "english":"Letter Award Fields",
+    "french":"Lettres bonus"
   },
   "Wird ein Buchstabe auf ein hellblaues Feld gesetzt, so verdoppelt sich sein Wert. Wird ein Buchstabe auf ein dunkelblaues Feld gesetzt, so verdreifacht sich sein Wert.":{
-    "english":"If a letter is placed on a light blue field, its value doubles. If a letter is placed on a dark blue field, its value triples."
+    "english":"If a letter is placed on a light blue field, its value doubles. If a letter is placed on a dark blue field, its value triples.",
+    "french":"Si une lettre est placée sur un champ bleu clair, sa valeur double. Si une lettre est placée sur un champ bleu foncé, sa valeur triple."
   },
   "Felder für Wortprämien":{
-    "english":"Word Premium Fields"
+    "english":"Word Premium Fields",
+    "french":"Mots bonus"
   },
   "Wird ein Buchstabe auf ein hellrotes Feld gesetzt, so verdoppelt sich der Wert des gesamten Wortes. Wird ein Buchstabe auf ein dunkelrotes Feld gesetzt, so verdreifacht sich der Wert des gesamten Wortes.":{
-    "english":"If a letter is placed on a light red field, the value of the entire word doubles. If a letter is placed on a dark red field, the value of the entire word is tripled."
+    "english":"If a letter is placed on a light red field, the value of the entire word doubles. If a letter is placed on a dark red field, the value of the entire word is tripled.",
+    "french":"Si une lettre est placée sur un champ rouge clair, la valeur du mot entier double. Si une lettre est placée sur un champ rouge foncé, la valeur du mot entier est triplée."
   },
   "Austausch von Buchstabensteinen":{
-    "english":"Replacement of letter stones"
+    "english":"Replacement of letter stones",
+    "french":"Remplacement des lettres"
   },
   "Ein Spieler hat die Möglichkeit einen oder all seine Buchstabensteine auszutauschen, wenn er an der Reihe ist. Er legt die Buchstabensteine mit dem Buchstaben nach unten zur Seite und zieht die gleiche Anzahl neuer Buchstabensteine aus dem Beutel. Die zur Seite gelegten Buchstabensteine werden dann in den Beutel gelegt und mit den anderen Steinen gemischt. Er muss diese Runde dann aussetzen.":{
     "german":"Du hast die Möglichkeit einen oder alle Buchstabensteine auszutauschen. Wähle die zu tauschenden Buchstaben aus, die gleiche Anzahl neuer Buchstabensteine bekommst du aus den übrigen Buchstaben. Die abgelegten Buchstabensteine werden dann zurück zum Vorrat gegeben. Du musst diese Runde dann aussetzen.",
-    "english":"You have the possibility to replace one or all of your letter stones when it is your turn. Select the letter stones to be abandoned. The same number of new letter stones will be drawn form the stash. The abandoned letter stones are then placed in the stash. You must then suspend this round."
+    "english":"You have the possibility to replace one or all of your letter stones when it is your turn. Select the letter stones to be abandoned. The same number of new letter stones will be drawn form the stash. The abandoned letter stones are then placed in the stash. You must then suspend this round.",
+    "french":"Vous avez la possibilité de remplacer une ou toutes vos pierres à lettres à votre tour. Sélectionnez les pierres à lettres à abandonner. Le même nombre de nouvelles pierres de lettre seront tirées de la cachette. Les pierres à lettres abandonnées sont ensuite placées dans la cachette. Vous devez ensuite suspendre ce tour."
   },
   "Du gewinnst.":{
-    "english":"You win."
+    "english":"You win.",
+    "french":"Vous avez gagné"
   },
   "Du verlierst.":{
-    "english":"You loose."
+    "english":"You loose.",
+    "french":"Vous avez perdu"
   }
 };
